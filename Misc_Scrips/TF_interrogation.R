88ae206cf363805159a2d934c20687e5c8cad8aa
JackLMc <jacklukemcmurray@gmail.com>
2019-11-13 11:18:27 +0000

    Small edits of paths

--- Misc_Scrips/TF_interrogation.R
+++ Misc_Scrips/TF_interrogation.R
@@ -1,9 +1,10 @@
 library(tidyverse)
+
 ## Looking at how many genes are Transcription factors
 VD1 <- read.csv("./Bulk/Output/CD27LO.vs.CD27HI.csv")
 VD1a <- droplevels(subset(VD1, adj.P.Val <= 0.05))
 
-TF <- read.csv("./Transcription_Factor/TFCheckpoint.csv")
+TF <- read.csv("./Data/TFCheckpoint.csv")
 colnames(TF)[colnames(TF) == "gene_symbol"] <- "SYMBOL"
 colnames(TF)[colnames(TF) == "entrez_human"] <- "ENTREZID"
 TF_imp <- droplevels(subset(TF, AnimalTFDB == "AnimalTFDB"))
@@ -12,13 +13,13 @@ VD1b <- merge(VD1a, TF_imp[, c("SYMBOL", "ENTREZID", "gene_name")],
                 by = c("SYMBOL", "ENTREZID")) %>% droplevels()
 
 VD1b$Direction <- ifelse((VD1b$logFC > 0), "VD1.CD27LO", "VD1.CD27HI")
-write.csv("./Transcription_Factor/VD1_DGE_TF.csv", x = VD1b, row.names = F)
+write.csv("./Output/TF/VD1_DGE_TF.csv", x = VD1b, row.names = F)
 
 ### CD8
 CD8 <- read.csv("./Bulk/Output/EMRA.vs.Naive.csv")
 CD8a <- droplevels(subset(CD8, adj.P.Val <= 0.05))
 
-TF <- read.csv("./Transcription_Factor/TFCheckpoint.csv")
+TF <- read.csv("./Data/TFCheckpoint.csv")
 colnames(TF)[colnames(TF) == "gene_symbol"] <- "SYMBOL"
 colnames(TF)[colnames(TF) == "entrez_human"] <- "ENTREZID"
 TF_imp <- droplevels(subset(TF, AnimalTFDB == "AnimalTFDB"))
@@ -27,7 +28,7 @@ CD8b <- merge(CD8a, TF_imp[, c("SYMBOL", "ENTREZID", "gene_name")],
               by = c("SYMBOL", "ENTREZID")) %>% droplevels()
 
 CD8b$Direction <- ifelse((CD8b$logFC > 0), "CD8.EMRA", "CD8.Naive")
-write.csv("./Transcription_Factor/CD8_DGE_TF.csv", x = CD8b, row.names = F)
+write.csv("./Output/TF/CD8_DGE_TF.csv", x = CD8b, row.names = F)
 
 load("./Bulk/Final_edgeR.RData")
 
@@ -44,8 +45,8 @@ sig$SYMBOL <- as.factor(sig$SYMBOL)
 TF_imp$SYMBOL <- as.factor(TF_imp$SYMBOL)
 tf_VD2_CD27HI <- droplevels(sig$SYMBOL[sig$SYMBOL %in% TF_imp$SYMBOL])
 
-write.csv("./Transcription_Factor/VD2_CD27LO.csv", x = tf_VD2_CD27LO)
-write.csv("./Transcription_Factor/VD2_CD27HI.csv", x = tf_VD2_CD27HI)
+write.csv("./Output/TF/VD2_CD27LO.csv", x = tf_VD2_CD27LO)
+write.csv("./Output/TF/VD2_CD27HI.csv", x = tf_VD2_CD27HI)
 
 
 # Comparisons and Colours
@@ -68,7 +69,7 @@ hclustAvg <- function(x) hclust(x, method = "average")
 ## v has VD2, v1 has not
 col.cell1 <- c("#56B4E9", "#E69F00", "#009E73", "#999999", "#CC79A7")[v1$targets$group]
 i <- which(v$genes$SYMBOL %in% VD1b$SYMBOL)
-pdf(file = "./Transcription_Factor/VD1_TF_heatmap.pdf", height = 8, width = 8)
+pdf(file = "./Figures/Transcription_Factor/VD1_TF_heatmap.pdf", height = 8, width = 8)
 heatmap.2(v1$E[i,], scale = "row",
           labRow = v$genes$SYMBOL[i], labCol = NA,
           col = mycol,
